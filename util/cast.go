120e274fdc9042c3e962eff6cb721b22516d19c8
alice52 <zzhang_xz@163.com>
2024-06-20 00:19:51 +0800

    fix(util): refine ptr and vl cast

--- util/cast.go
+++ util/cast.go
@@ -4,6 +4,11 @@ func ToPrt[T any](i T) *T {
 	return &i
 }
 
+// ToVl 返回指针i指向的值，如果i是nil，则返回T的零值。
 func ToVl[T any](i *T) T {
+	if i == nil {
+		var zero T
+		return zero
+	}
 	return *i
 }
--- util/cast_test.go
+++ util/cast_test.go
@@ -14,3 +14,16 @@ func TestToPrt(t *testing.T) {
 	fmt.Println(ToPrt(func() {}))
 	fmt.Println(ToPrt(struct{}{}))
 }
+
+func TestToVl(t *testing.T) {
+	var a *int64
+	fmt.Println(ToVl(a))
+	var b *string
+	fmt.Println(ToVl(b))
+	fmt.Println(ToVl(ToPrt(1)))
+	fmt.Println(ToVl(ToPrt(true)))
+	fmt.Println(ToVl(ToPrt([]int{1, 2, 3})))
+	fmt.Println(ToVl(ToPrt(map[string]int{"a": 1, "b": 2})))
+	//fmt.Println(ToVl(ToPrt(func() {})))
+	fmt.Println(ToVl(ToPrt(struct{}{})))
+}
