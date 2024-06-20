bf52f37d47574f8bab43572354d8a8fd3f07e0ab
alice52 <zzhang_xz@163.com>
2024-06-21 00:19:43 +0800

    fix(ip): refine ip valid

--- ip/valid.go
+++ ip/valid.go
@@ -1,9 +1,19 @@
 package ip
 
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 // IsValidIPv4 checks if the provided string is a valid IPv4 address.
 func IsValidIPv4(ip string) bool {
+
+	if strings.HasPrefix(ip, "10.") ||
+		strings.HasPrefix(ip, "192.") ||
+		strings.HasPrefix(ip, "127.") {
+		return false
+	}
+
 	parsedIP := net.ParseIP(ip)
 	if parsedIP == nil {
 		return false
