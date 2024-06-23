3331095cb2ae42c2b0dbba95c9c721d209c80a4b
alice52 <zzhang_xz@163.com>
2024-06-23 20:04:00 +0800

    feat(gz): add gz mw and copier with time, map

--- util/time.go
+++ util/time.go
@@ -8,7 +8,7 @@ func Parse(tStr string) (time.Time, error) {
 
 func TryParse(tStr string) time.Time {
 	if t, err := time.Parse("2006-01-02T15:04:05.000Z", tStr); err != nil {
-		t, _ := time.Parse("2006-01-02T15:04:05.000Z", tStr)
+		t, _ := time.Parse("2006-01-02 15:04:05", tStr)
 		return t
 	} else {
 		return t
@@ -17,7 +17,7 @@ func TryParse(tStr string) time.Time {
 
 func MustParse(tStr string) time.Time {
 	if t, err := time.Parse("2006-01-02T15:04:05.000Z", tStr); err != nil {
-		t, err := time.Parse("2006-01-02T15:04:05.000Z", tStr)
+		t, err := time.Parse("2006-01-02 15:04:05", tStr)
 		if err != nil {
 			panic(err)
 		} else {
@@ -28,16 +28,50 @@ func MustParse(tStr string) time.Time {
 	}
 }
 
-func Format(t *time.Time) string {
+// Deprecated: Format
+func FormatVal(t time.Time) string {
+
+	if t.IsZero() {
+		return ""
+	}
+
+	return t.Format("2006-01-02T15:04:05.000Z")
+}
+
+// Deprecated: Format
+func Format1(t *time.Time) string {
 	if t == nil {
 		return ""
 	}
 	return t.Format("2006-01-02T15:04:05.000Z")
 }
 
+// Deprecated: Format
 func FormatV0(t *time.Time) string {
 	if t == nil {
 		return ""
 	}
 	return t.Format("2006-01-02 15:04:05")
+}
+
+// Format 此时直接出入 nil 会报错
+func Format[T *time.Time | time.Time](t T, format ...string) string {
+	var f = "2006-01-02T15:04:05.000Z"
+	if len(format) > 0 {
+		f = format[0]
+	}
+	switch v := any(t).(type) {
+	case time.Time:
+		if v.IsZero() {
+			return ""
+		}
+		return v.Format(f)
+	case *time.Time:
+		if v == nil || v.IsZero() {
+			return ""
+		}
+		return v.Format(f)
+	default:
+		return ""
+	}
 }
