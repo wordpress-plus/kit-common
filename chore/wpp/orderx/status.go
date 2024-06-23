9b17573a93f35f3e6b648a0f7647e25e9bab7fa2
alice52 <zzhang_xz@163.com>
2024-06-23 20:04:00 +0800

    feat(gz): add gz mw and copier with time, map

---
+++ chore/wpp/orderx/status.go
@@ -0,0 +1,19 @@
+package x
+
+const (
+	StatusOrdered   = 0
+	StatusUnpaid    = 1
+	StatusPaid      = 2
+	StatusUnshipped = 3
+	StatusShipping  = 4
+	StatusShipped   = 5
+	StatusConfirmed = 6
+	StatusUnrated   = 7
+	StatusRated     = 8
+	StatusCompleted = 9
+	StatusCanceled  = 10
+	StatusClosed    = 11
+	StatusDeleted   = 12
+	StatusReturning = 13
+	StatusReturned  = 14
+)
